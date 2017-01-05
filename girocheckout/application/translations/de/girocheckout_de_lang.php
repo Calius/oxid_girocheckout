b48c84f49c66eb7b1ac867da48e8d761ad1e91d3
Christian Lewin <cl@heidemannsound.de>
2017-01-05 15:25:45 +0100

    Fix wrong translation path for newer oxid versions

---
+++ girocheckout/application/translations/de/girocheckout_de_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$aLang = array(
+    'charset' => 'utf-8',
+    // GiroCheckout module
+    'GC_ACCOUNT' => 'Kontonummer',
+    'GC_BANKCODE' => 'Bankleitzahl',
+    'GC_ACCOUNTHOLDER' => 'Kontoinhaber',
+    'GC_BIC' => 'Ihre Bank',
+    'GC_IBAN' => 'IBAN',
+    'GC_BLZ' => 'Kontonummer/BLZ verwenden',
+    'GC_ISSUERS' => 'Bank',
+    'GC_CREDITCARD_DESCRIPTION' => 'Im nächsten Schritt werden Sie aufgefordert, Ihre Kreditkartendaten einzugeben. Wenn Ihre Kreditkarte für 3D-Secure (Verified by Visa bzw. MasterCard SecureCode) freigeschaltet ist, werden Sie während des Bezahlprozesses zur Sicherheitsabfrage Ihrer Bank weitergeleitet.',
+    'GC_GIROPAY_DESCRIPTION' => 'Mit giropay zahlen Sie einfach, schnell und sicher im Online-Banking Ihrer teilnehmenden Bank oder Sparkasse. Sie werden direkt zum Online-Banking Ihrer Bank weitergeleitet, wo Sie die Überweisung durch Eingabe von PIN und TAN freigeben.',
+    'GC_DIRECTDEBIT_DESCRIPTION' => 'Hiermit ermöchtigen Sie uns, den fälligen Betrag von Ihrem Konto mittels Lastschrift einzuziehen.',
+    'GC_IDEAL_DESCRIPTION' => 'iDEAL ist das Online Überweisungssystem der niederländischen Banken, das auf dem etablierten Onlinebanking-Verfahren der Kreditinstitute basiert. Im folgenden müssen Sie Ihre Bank auswählen und können anschließend die Zahlung in Ihrem Online-Banking durchführen.',
+    'GC_EPS_DESCRIPTION' => 'Mit eps Online-Überweisung zahlen Sie einfach, schnell und sicher im Online-Banking Ihrer Bank. Im nächsten Schritt werden Sie direkt zum Online-Banking Ihrer Bank weitergeleitet, wo Sie die Zahlung durch Eingabe von PIN und TAN freigeben.',
+    'GC_PAYDIREKT_DESCRIPTION' => 'Das Online-Bezahlverfahren Ihrer Banken und Sparkassen. Sie werden auf die Webseiten von Paydirekt weiter geleitet, wo Sie sich mit Ihren persönlichen Zugangsdaten anmelden und dann die Zahlung bestätigen. Sie benötigen vorab eine Registrierung bei Paydirekt.',
+    'GC_SOFORTUW_DESCRIPTION' => 'Mit SOFORT Überweisung geht das online Bezahlen einfach wie nie! Sie nutzen dabei ganz bequem Ihre vertrauten Online-Banking Daten und sind so auf der sicheren Seite. Sie werden auf die SOFORT-Webseiten weiter geleitet, geben dort Ihre Bankdaten ein und lösen die Online-Überweisung aus.',
+    'GC_BIC_TEXTINPUTINFO' => '(Suchen Sie nach Bankname, BLZ, Ort oder BIC)',
+	
+);
---
+++ girocheckout/application/translations/en/girocheckout_en_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$aLang = array(
+    'charset' => 'utf-8',
+    // GiroCheckout module
+    'GC_ACCOUNT' => 'Bank account',
+    'GC_BANKCODE' => 'Bank code',
+    'GC_ACCOUNTHOLDER' => 'Account holder',
+    'GC_BIC' => 'Bank',
+    'GC_IBAN' => 'IBAN',
+    'GC_BLZ' => 'Bank account / Bank code',
+    'GC_ISSUERS' => 'Issuer',
+    'GC_CREDITCARD_DESCRIPTION' => 'On the following secure website, you are asked to enter your credit card data. If your credit card is enabled for 3D Secure (Verified by Visa or MasterCard Secure Code), additional information will be passed for a security check at your bank.',
+    'GC_GIROPAY_DESCRIPTION' => 'With giropay you pay simply, fast and secure by the online banking of your participating bank. You will be redirected to the online banking of your bank where you authorize the credit transfer.',
+    'GC_DIRECTDEBIT_DESCRIPTION' => 'Hereby you authorize us to charge your current account with the invoiced amount by direct debit.',
+    'GC_IDEAL_DESCRIPTION' => 'With iDEAL you pay simply, fast and secure by online banking of your bank. You will be redirected to the online banking of your bank where you authorize the credit transfer with PIN and TAN.',
+    'GC_EPS_DESCRIPTION' => 'With eps online transfer you pay simply, fast and secure by online banking of your bank. Your will be redirected to the online banking at your bank where you authorize the credit transfer with PIN and TAN.',
+    'GC_PAYDIREKT_DESCRIPTION' => 'The online payment method of the German banks and Sparkassen. You will be redirected to the Paydirekt web site, where you will log into your personal account and authorize the payment. You require prior registration with Paydirekt. This payment method only works with German banking accounts.',
+    'GC_SOFORTUW_DESCRIPTION' => 'Direct payment via online banking. Make payments the easy way with your own online banking details – fast and direct. You will be redirected to the web site of SOFORT, where you will enter your banking data and initiate the electronic wire transfer.',
+    'GC_BIC_TEXTINPUTINFO' => '(search for bank name, BLZ, city or BIC)',
+);
